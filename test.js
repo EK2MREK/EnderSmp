85bfd9c4cdc6fbc46f68611205445ca1eed92079
EK2MREK <120745891+EK2MREK@users.noreply.github.com>
2022-12-16 20:14:58 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,33 @@
+let Creator = {
+    name: "Creator: EnderKing",
+    age: "Creator: Age: 8",
+}
+
+// DOT NOTATION
+// BRACKET NOTATION
+Creator['name'] = "EnderKing";
+
+console.log(Creator.name)
+console.log(Creator.age);
+let names = ["EnderKing"];
+
+console.log(names);
+
+
+// task
+function Subscribe() {
+    
+console.log("Go Subscribe To The Creator youtube.com/channel/UCvDaNYFz--JgPG2Xs8NHSJQ")
+
+}
+
+Subscribe();
+
+// task
+function Welcome(name) {
+    
+    console.log("Welcome  "  + name);
+    
+    }   
+    
+    Welcome("Sir");
